a08e929426cd179a36231e56ae71c6748e18c3a9
jedermann <johannesklinger@web.de>
2015-04-29 14:14:33 +0100

    Adds calcBmis function with function in where binding

--- functions.hs
+++ functions.hs
@@ -60,3 +60,7 @@ a `compare'` b
 
 initials :: String -> String -> String
 initials (f:_) (l:_) = [f] ++ ". " ++ [l] ++"."
+
+calcBmis :: (RealFloat a) => [(a, a)] -> [a]
+calcBmis xs = [bmi w h | (w, h) <- xs]
+    where bmi weight height = weight / height ^ 2
