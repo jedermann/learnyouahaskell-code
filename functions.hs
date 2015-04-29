ad638e87ee1ce1d3f457d64ece613d50d6e6489f
jedermann <johannesklinger@web.de>
2015-04-29 14:34:47 +0100

    changes describeList to use pattern matching where binding

--- functions.hs
+++ functions.hs
@@ -71,6 +71,7 @@ cylinder r h =
       in sideArea + 2 * topArea
 
 describeList :: [a] -> String
-describeList xs = "This list is " ++ case xs of []  -> "empty"
-                                                [x] -> "a singleton list"
-                                                xs  -> "a longer list"
+describeList xs = "This list is " ++ what xs
+    where what [] = "empty"
+          what [x] = "a singleton list"
+          what xs = "a longer list"
