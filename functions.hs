7caf93f569a322f2f2adaeaaff36a64879e52a4a
jedermann <johannesklinger@web.de>
2015-04-29 14:10:36 +0100

    Pattern matching for initials directly in parameters

--- functions.hs
+++ functions.hs
@@ -59,6 +59,4 @@ a `compare'` b
     | otherwise = LT
 
 initials :: String -> String -> String
-initials firstname lastname = [f] ++ ". " ++ [l] ++"."
-    where (f:_) = firstname
-          (l:_) = lastname
+initials (f:_) (l:_) = [f] ++ ". " ++ [l] ++"."
